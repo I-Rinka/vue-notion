30a9b7d11c9b2cd3ed1a1f3a645577fb47def09f
I-Rinka <I_Rinka@outlook.com>
2022-04-07 20:05:49 +0800

    adaptor OK

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,16 +1,28 @@
 <template>
   <div id="app">
     <button @click="GetData">get data</button>
-    <NotionRenderer :blockMap="blockMap" fullPage prism katex todo />
+    <!-- <NotionRenderer :blockMap="blockMap" fullPage prism katex todo /> -->
+    <div class="posts">
+      <h2>All Posts</h2>
+      <ul>
+        <li v-for="(post, k) in posts" :key="k">
+          <div>{{ post }}</div>
+          <!-- <NuxtLink v-if="post.id" :to="post.id" class="button--grey"> -->
+          <!-- <b>{{ post.Name }}</b> -->
+          <!-- </NuxtLink> -->
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import { NotionRenderer, getPageBlocks, getDatabase } from "@/entry";
+import { NotionRenderer, DatabaseItemsAdaptor, queryDatabase } from "@/entry";
 
 import "prismjs";
 import "prismjs/themes/prism.css";
 import "katex/dist/katex.min.css";
+import { getPageTable } from "../src/lib/api";
 
 export default {
   name: "ServeDev",
@@ -18,11 +30,11 @@ export default {
     NotionRenderer,
   },
   data() {
-    return { blockMap: null };
+    return { blockMap: null, posts: null };
   },
   methods: {
     async GetData() {
-      const data = await getDatabase("6c12515ae1d64423839e4540cacf49a5");
+      const data = await queryDatabase("6c12515ae1d64423839e4540cacf49a5");
       console.log(data);
     },
   },
@@ -32,8 +44,12 @@ export default {
     // sync tester: c10e1556bec44f23a20f71497b3d98c4
     // table tester: bd1de400a8b349dc824f4f00e61d0797
     // todo tester: 235057194b954a60ace89c052a65d102
-    this.blockMap = await getPageBlocks("2e22de6b770e4166be301490f6ffd420");
-
+    // this.blockMap = await getPageBlocks("2e22de6b770e4166be301490f6ffd420");
+    // let pageTable = await getPageTable("6c12515ae1d64423839e4540cacf49a5");
+    let pageTable = DatabaseItemsAdaptor(
+      await queryDatabase("6c12515ae1d64423839e4540cacf49a5")
+    );
+    this.posts = pageTable.filter((page) => page.Published);
   },
 };
 </script>
--- src/lib/notion.js
+++ src/lib/notion.js
@@ -7,52 +7,59 @@ const notion = new Client({
   baseUrl: config.NOTION_PROXY + "https://api.notion.com"
 });
 
-export const getDatabase = async (databaseId, apiUrl = config.NOTION_API) => {
-  (async () => {
-    const response = await notion.databases.query({
-      database_id: databaseId
-    });
-    console.log(response);
-  })();
+export const queryDatabase = async (databaseId, apiUrl = config.NOTION_API) => {
+  return await notion.databases.query({
+    database_id: databaseId
+  });
 
-  const myHeaders = new Headers();
-  myHeaders.append("Origin", config.NOTION_API);
-  myHeaders.append("authorization", "Bearer " + config.NOTION_KEY);
-  myHeaders.append("Notion-Version", config.NOTION_API_VERSION);
-  return await fetch(config.NOTION_PROXY + `${apiUrl}databases/${databaseId}`, {
-    headers: myHeaders
-  }).then(res => res.json());
+  // const myHeaders = new Headers();
+  // myHeaders.append("Origin", config.NOTION_API);
+  // myHeaders.append("authorization", "Bearer " + config.NOTION_KEY);
+  // myHeaders.append("Notion-Version", config.NOTION_API_VERSION);
+  // return await fetch(config.NOTION_PROXY + `${apiUrl}databases/${databaseId}`, {
+  //   headers: myHeaders
+  // }).then(res => res.json());
 };
 
-/*  await fetch(`${apiUrl}/databases/${}`,
-    {
-      headers: new Headers(
-        {
-          'authorization': 'Bearer '+config.NOTION_KEY,
-          'Notion-Version': config.NOTION_API_VERSION,
-        }
-      )
-    }
-  ).then(res => res.json());
-*/
-// export const getPage = async pageId => {
-//   const response = await notion.pages.retrieve({ page_id: pageId });
-//   return response;
-// };
+export function DatabaseItemsAdaptor(listObject) {
+  // go to property
+  if (listObject.object !== "list") {
+    console.log(listObject);
+    return [];
+  }
+  return listObject.results.map(value => {
+    let page = {
+      created_time: value.created_time,
+      last_edited_time: value.last_edited_time,
+      id: value.id
+    };
+    for (const key in value.properties) {
+      if (Object.hasOwnProperty.call(value.properties, key)) {
+        const element = value.properties[key];
+        if (element.type === "checkbox") {
+          page[key] = element.checkbox;
+        } else if (element.type === "multi_select") {
+          page[key] = element.multi_select;
+        } else if (element.type === "title") {
+          let text = "";
+          let newId = value.id;
 
-// export const getBlocks = async blockId => {
-//   const blocks = [];
-//   let cursor;
-//   while (true) {
-//     const { results, next_cursor } = await notion.blocks.children.list({
-//       start_cursor: cursor,
-//       block_id: blockId
-//     });
-//     blocks.push(...results);
-//     if (!next_cursor) {
-//       break;
-//     }
-//     cursor = next_cursor;
-//   }
-//   return blocks;
-// };
+          for (let index = 0; index < element.title.length; index++) {
+            const title = element.title[index];
+            if (title.type === "mention") {
+              if (title.mention.type === "page") {
+                newId = title.mention.page.id;
+              }
+            }
+            text += title.plain_text;
+          }
+          page[key] = text;
+          page.id = newId;
+        } else {
+          page[key] = "❌currently unsupport " + element.type;
+        }
+      }
+    }
+    return page;
+  });
+}
