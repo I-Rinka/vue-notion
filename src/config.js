a8e8ab851649930f839158a4d9c9e8d0120917f7
I-Rinka <I_Rinka@outlook.com>
2022-04-09 15:13:56 +0800

    new structure

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const config = {
   NOTION_KEY: "secret_",
+  NOTION_HOST: "https://api.notion.com",
   NOTION_API: "https://api.notion.com/v1/",
-  NOTION_PROXY: "http://localhost:8081/",
+  NOTION_PROXY: "https://cors-anywhere.herokuapp.com",
   NOTION_API_VERSION: "2022-02-22"
 };
